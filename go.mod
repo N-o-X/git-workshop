d0cb8e2b10efc4928bd05399e964907ecbf5ec13
Noah Hilverling <noah.hilverling@nitrado.net>
2023-07-27 15:56:27 +0200

    Add code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module git.marbis.net/noah.hilverling/git-training-project
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "fmt"
+    "sort"
+    "sync"
+)
+
+func main() {
+    var ids []int
+    lock := sync.Mutex{}
+    wg := sync.WaitGroup{}
+
+    for i := 1; i <= 100; i++ {
+        wg.Add(1)
+        i := i
+        go func() {
+            lock.Lock()
+            ids = append(ids, i)
+            lock.Unlock()
+            wg.Done()
+        }()
+    }
+
+    wg.Wait()
+    sort.Ints(ids)
+    fmt.Println(ids)
+}
